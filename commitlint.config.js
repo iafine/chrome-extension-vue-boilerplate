4dccd8e8c65d9c9a8c613ebae3ec9e84c6bf4bd2
iafine <iafineden@gmail.com>
2021-09-28 15:52:39 +0800

    feat: 优化lint工具

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional
+  /**
+  'build',
+  'chore',
+  'ci',
+  'docs',
+  'feat',
+  'fix',
+  'perf',
+  'refactor',
+  'revert',
+  'style',
+  'test'
+   */
+  extends: ['@commitlint/config-conventional']
+}
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,42 +1,46 @@
 import $ from 'jquery'
 
+chrome.storage.local.get('SET', function (data) {
+  const SET = data.SET
+  if (SET) {
+    const username = SET.username
+    const password = SET.password
+    const skus = SET.skus
+    const state = SET.state
 
-chrome.storage.local.get('SET', function(data) {
-  let SET = data.SET;
-  if(SET){
-    let username = SET.username;
-    let password = SET.password;
-    let skus = SET.skus;
-    let state = SET.state;
-
-    $('input[name="username"]').val(username);
-    $('input[name="password"]').val(password);
-    $('textarea[name="skus"]').val(skus);
-    $("input[type='checkbox'][name='state']").prop("checked", state);
-  }else{
+    $('input[name="username"]').val(username)
+    $('input[name="password"]').val(password)
+    $('textarea[name="skus"]').val(skus)
+    $("input[type='checkbox'][name='state']").prop('checked', state)
+  } else {
     // $("input[type='checkbox'][name='state']").prop("checked", true);
   }
 })
 
-$(document).on('click','.save',function(){
-  SetData('');
+$(document).on('click', '.save', function () {
+  SetData('')
 })
 
-$(document).on('click','.buy',function(){
-  SetData('https://www.ti.com.cn/zh-cn/ordering-resources/buying-tools/quick-add-to-cart.html');
+$(document).on('click', '.buy', function () {
+  SetData(
+    'https://www.ti.com.cn/zh-cn/ordering-resources/buying-tools/quick-add-to-cart.html'
+  )
 })
 
-function SetData(url: any) {
-  let username = $('input[name="username"]').val();
-  let password = $('input[name="password"]').val();
-  let skus = $('textarea[name="skus"]').val();
-  let state = $("input[type='checkbox'][name='state']").prop("checked");
+function SetData(url: string) {
+  const username = $('input[name="username"]').val()
+  const password = $('input[name="password"]').val()
+  const skus = $('textarea[name="skus"]').val()
+  const state = $("input[type='checkbox'][name='state']").prop('checked')
   // var state = $("input[type='checkbox'][name='state']").prop("checked");
-  chrome.storage.local.set({
-    'SET': { username:username,password:password,skus:skus, state: state}
-  }, function () {
-    if(url!=''){
-      window.open(url);
+  chrome.storage.local.set(
+    {
+      SET: { username: username, password: password, skus: skus, state: state }
+    },
+    function () {
+      if (url != '') {
+        window.open(url)
+      }
     }
-  })
+  )
 }
